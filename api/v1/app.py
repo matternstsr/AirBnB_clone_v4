d1c8e19cca69c54aedba607231d25175f5e6882a
matternstsr <matternstsr@gmail.com>
2023-12-09 11:50:16 -0600

    added console logging for testing and fixed the css

--- api/v1/app.py
+++ api/v1/app.py
@@ -43,7 +43,7 @@ Swagger(app)
 if __name__ == "__main__":
     """ Main Function """
     host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
+    port = environ.get('HBNB_API_PORT') or '5000'
     if not host:
         host = '0.0.0.0'
     if not port:
